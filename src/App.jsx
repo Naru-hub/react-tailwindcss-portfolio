42c2c4bf0113b86b648145c3e8e97e5fa88f78ed
Naru-hub <1208ny0324@gmail.com>
2022-12-10 19:51:36 +0900

    section部分のHTMLを作成

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,42 @@ import "./App.css";
 
 function App() {
   return (
-    <header className="text-gray-500 border-b border-gray-200">
-      <div className="container flex mx-auto p-5 flex-col md:flex-row items-center">
-        <a href="#" className="font-medium text-gray-800 mb-4 md:mb-0">
-          <span className="text-xl ml-3">Nona</span>
-        </a>
-        <nav className="md:ml-auto text-base">
-          <a href="#Home" className="mr-5 hover:text-blue-400 duration-300">
-            Home
+    <>
+      <header className="text-gray-500 border-b border-gray-200">
+        <div className="container flex mx-auto p-5 flex-col md:flex-row items-center">
+          <a href="#" className="font-medium text-gray-800 mb-4 md:mb-0">
+            <span className="text-xl ml-3">Nona</span>
           </a>
-          <a href="#About" className="mr-5 hover:text-blue-400 duration-300">
-            about
-          </a>
-          <a href="#Skills" className=" hover:text-blue-400 duration-300">
-            skills
-          </a>
-        </nav>
-      </div>
-    </header>
+          <nav className="md:ml-auto text-base">
+            <a href="#Home" className="mr-5 hover:text-blue-400 duration-300">
+              Home
+            </a>
+            <a href="#About" className="mr-5 hover:text-blue-400 duration-300">
+              about
+            </a>
+            <a href="#Skills" className=" hover:text-blue-400 duration-300">
+              skills
+            </a>
+          </nav>
+        </div>
+      </header>
+      <section>
+        <div>
+          <div>
+            <h1>
+              Hi!
+              <br />
+              I'm Nona
+            </h1>
+            <p>Web Developer</p>
+            <button>Contact Me</button>
+          </div>
+          <div>
+            <img src="./img/icon.jpeg" alt="" />
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
